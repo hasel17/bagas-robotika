96f6f9a5314c6b6e0c06f2cf49e5b9fb25dd5c85
hasel17 <bagasfabianmaulana@gmail.com>
2024-04-27 02:22:35 +0700

    Tugas 6

---
+++ Tugas (Week) 6/rns/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/nazrin/rns/src
+# Build directory: /home/nazrin/rns/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("differential_drive_robot_description")
+subdirs("navigation")
