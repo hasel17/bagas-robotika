1081d34d5a6da313702b07b124a1add2dcf1e912
hasel17 <bagasfabianmaulana@gmail.com>
2024-04-26 12:29:00 +0700

    pengerjaan tugas 3

---
+++ Tugas (Week) 3/subscriber_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int32
+
+nodeName='messagesubscriber'
+
+topicName='information'
+
+def callbackFunction(message):
+	print("We received %d"%message.data)
+
+rospy.init_node(nodeName)
+
+rospy.Subscriber(topicName, Int32, callbackFunction)
+
+rospy.spin()
+
